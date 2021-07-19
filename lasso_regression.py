3248331ed3477e7a119dad9e49b8fa5ce38d11e7
Christian Scott <73006531+christianscooty@users.noreply.github.com>
2021-07-19 17:12:09 -0400

    Update lasso_regression.py

--- lasso_regression.py
+++ lasso_regression.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 
 import pandas as pd
-from numpy import arange
+import numpy as np
+from sklearn import linear_model
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import Lasso
-from sklearn.model_selection import RepeatedKFold
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 #%%
 data = pd.read_csv("data.csv")
 
@@ -15,14 +18,34 @@ print(data.keys())
 #%% dropping unncessary columns
 data.drop(['Unnamed: 0','Id','groupId', 'matchId','matchType'],axis = 1, inplace = True)
 
+#%% 
+data.isna().sum()
+
 #%%
 subset = data.head(50000)
+subset = pd.DataFrame(subset)
 print(subset.shape)
 
 #%%
-subset = subset.dropna()
-subsets =subset.astype('int')
+subset.info()
+subsets =subset.astype('float')
 subsets.info()
+
+#%%
+
+corr = subsets.corr()
+mask = np.triu(np.ones_like(corr, dtype=bool))
+
+#%%
+# Set up the matplotlib figure
+f, ax = plt.subplots(figsize=(11, 9))
+
+# Generate a custom diverging colormap
+cmap = sns.diverging_palette(230, 20, as_cmap=True)
+
+# Draw the heatmap with the mask and correct aspect ratio
+sns.heatmap(corr, mask=mask, cmap=cmap, vmax=.3, center=0,
+            square=True, linewidths=.5, cbar_kws={"shrink": .5})
 
 #%%
 scaler = StandardScaler()
@@ -40,10 +63,26 @@ reg = Lasso(alpha=0.001, max_iter = 5000)
 #%%
 reg.fit(X_train,Y_train)
 
+
 #%%
 
 print('Lasso Regression: R^2 score on training set', reg.score(X_train, Y_train)*100)
 print('Lasso Regression: R^2 score on test set', reg.score(X_test, Y_test)*100)
 
+#%% feature importance
+
+print(reg.coef_)
+coefficients = reg.coef_
+coefficients = pd.Series(coefficients)
+results = pd.DataFrame()
+results = results.assign(names = subset.keys(), coefficients = coefficients)
+
+#%% A Second way to do the same thing
+lasso_reg = linear_model.Lasso(alpha = .001, max_iter = 1000, tol = 0.01)
+lasso_reg.fit(X_train, Y_train)
+
+#%%
+print(lasso_reg.score(X_test,Y_test))
+print(lasso_reg.score(X_train,Y_train))
 
 
